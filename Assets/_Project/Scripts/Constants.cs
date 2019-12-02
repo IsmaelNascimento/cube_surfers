9bd8fa27b26fdd21f5b1b1a0355439dfdc3a583f
Ismael Nascimento <contato@ismaelnascimento.com>
2019-12-02 03:12:23 -0300

    [ADDED] Google signin system

--- Assets/_Project/Scripts/Constants.cs
+++ Assets/_Project/Scripts/Constants.cs
@@ -5,6 +5,7 @@
     public const string ENDPOINT_GET_PEOPLE = "https://uinames.com/api/?amount=500";
     public const int INCREASE_POINT_COUNT = 1;
     public const string HIGHSCORE_PLAYERPREFS = "HIGHSCORE";
+    public const string WEB_CLIENT_ID = "574528969942-jda0u9intkrv3mf4mkdgqg07hb946qi7.apps.googleusercontent.com";
 
     #endregion
 }
